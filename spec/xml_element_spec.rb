494ba091d6bd634d1afd3a5fd6310d751bfefda6
Clemens Helm <clemens.helm@gmail.com>
2014-07-09 15:46:48 +0200

    Query sub-elements with xpath.

--- spec/xml_element_spec.rb
+++ spec/xml_element_spec.rb
@@ -4,7 +4,7 @@ describe "GDataXMLElement" do
     element.to_s.should == "Hello World"
     element.childCount.should == 1
   end
-  
+
   it "should read and write attribute and value" do
     element = GDataXMLElement.with_xml("<a href='http://www.google.com'>Google</a>")
     element["href"].should == "http://www.google.com"
@@ -12,5 +12,15 @@ describe "GDataXMLElement" do
     element["href"] = "http://wikipedia.org"
     element.stringValue = "Wikipedia"
     element.to_xml.should == '<a href="http://wikipedia.org">Wikipedia</a>'
+  end
+
+  it "should query sub-elements" do
+    doc = GDataXMLDocument.with_html <<-HTML
+      <div><p>You found me!</p></div>
+      <p>I hope, you didn't find me!</p>
+    HTML
+    element = doc.xpath("//div").first
+
+    element.xpath("//p").map(&:stringValue).join('').should == "You found me!"
   end
 end
